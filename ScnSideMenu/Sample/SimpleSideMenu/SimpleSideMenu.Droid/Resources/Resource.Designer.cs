2551a4c0f3aebfc26bb1b49af564a9d9f7b0aab3
Maxim Levsha <mliausha@mliausha.scnsoft.com>
2015-07-14 18:30:44 +0300

    Updated Xamarin.Forms 1.4.3.6376 Created package 1.0.1

--- ScnSideMenu/Sample/SimpleSideMenu/SimpleSideMenu.Droid/Resources/Resource.Designer.cs
+++ ScnSideMenu/Sample/SimpleSideMenu/SimpleSideMenu.Droid/Resources/Resource.Designer.cs
@@ -28,6 +28,8 @@ namespace SimpleSideMenu.Droid
 		{
 			global::ScnGesture.Plugin.Droid.Resource.String.ApplicationName = global::SimpleSideMenu.Droid.Resource.String.ApplicationName;
 			global::ScnGesture.Plugin.Droid.Resource.String.Hello = global::SimpleSideMenu.Droid.Resource.String.Hello;
+			global::Xamarin.Forms.Platform.Resource.String.ApplicationName = global::SimpleSideMenu.Droid.Resource.String.ApplicationName;
+			global::Xamarin.Forms.Platform.Resource.String.Hello = global::SimpleSideMenu.Droid.Resource.String.Hello;
 		}
 		
 		public partial class Attribute
